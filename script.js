ff1eea9eef1aa573fd88500faa523d01cfab65f3
unknown <enchan101@gmail.com>
2024-07-13 23:34:50 -0400

    added project tracker

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 // script.js
 
 document.addEventListener('DOMContentLoaded', function() {
-    const inputBox = document.getElementById('text-input');
+    const inputBox = document.getElementById('homepage-input');
+    // Example: Add event listener for input event
+    inputBox.addEventListener('input', function(event) {
+        console.log('Input value:', inputBox.value);
+        // Example: Limit input to 10 characters
+        if (inputBox.value.length > 8) {
+            inputBox.value = "";
+        }
+    });
+});
 
-
+document.addEventListener('DOMContentLoaded', function() {
+    const inputBox = document.getElementById('spreadsheet-input');
     // Example: Add event listener for input event
     inputBox.addEventListener('input', function(event) {
         console.log('Input value:', inputBox.value);
